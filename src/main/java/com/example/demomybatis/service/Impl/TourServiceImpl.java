c33c67a25724de595ff6e7d8da8a9cfe9b6ead98
tzhang996527@hotmail.com <Hjk123456>
2019-05-26 09:13:48 +0800

    add tour

--- src/main/java/com/example/demomybatis/service/Impl/TourServiceImpl.java
+++ src/main/java/com/example/demomybatis/service/Impl/TourServiceImpl.java
@@ -4,9 +4,12 @@ import com.example.demomybatis.entity.Tour;
 import com.example.demomybatis.dao.TourMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+//@Transactional()//spring boot 会自动配置一个 DataSourceTransactionManager
 @Service("tourService")
 public class TourServiceImpl implements TourService {
 
@@ -18,8 +21,17 @@ public class TourServiceImpl implements TourService {
     }
 
     @Override
+    @Transactional(readOnly = true,propagation = Propagation.REQUIRED)
     public List<Tour> getAllTours() {
         return tourMapper.selectTour();
+        //如果异常被抓起之后，需要回滚只能手动回滚，否则事务会认为异常已经被处理，就不在进行回滚
+//        try {
+//            int i=1/0;//抛出异常
+//        } catch (Exception e) {
+//            //事务回滚
+//            TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
+//        }
+        //如果异常没有被抓起，则自动回滚
     }
 }
 
